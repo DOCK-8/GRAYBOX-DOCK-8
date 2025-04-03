6ceddc7473195cd6d28468bd8f3380717aa15467
DOCK <franwillaa@gmail.com>
2025-04-02 21:09:17 -0500

    created laboratory enviroment

---
+++ introduction-testing.js
@@ -0,0 +1,15 @@
+//RECOMEND LIBRARY
+const asar = require('@electron/asar');
+const fs = require('fs');
+//FILE TO EXAMINE
+const home = __dirname;
+const file = home + '/testing/test.asar';
+/*
+--COMPLETE THE CODE INTO ./CREDENTIALVERIFICATE/INDEX.JS--
+1. create de container with de docker-compose
+2. go into the credentialVerificate folder
+3. create index.js file
+--TEST YOUR CODE--
+A. reload the container with the docker-compose
+B. destroy the container with the docker-compose and create it again
+*/
